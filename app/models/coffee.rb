debfdd809f7b48222580226748fb0bb69d55f783
desmond107 <desmondchonte@gmail.com>
2022-07-30 13:22:49 +0300

    add relationship macros

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,3 +1,4 @@
 class Coffee < ActiveRecord::Base
-  belongs_to :user
+    has_many :orders
+    has_many :users, through: :orders
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ActiveRecord::Base
   belongs_to :user
-  has_many :coffees, through: :user
+  belongs_to :coffee
 end
